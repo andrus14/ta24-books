0c0acec682f2ca003684265104649320760bda63
Andrus <andrus.naulainen@ametikool.ee>
2025-10-24 14:07:18 +0300

    Refactor book.php to display book details and authors in a structured format

--- book.php
+++ book.php
@@ -13,4 +13,32 @@ $stmt = $pdo->prepare('SELECT * FROM books WHERE id = :id');
 $stmt->execute(['id' => $id]);
 $book = $stmt->fetch();
 
-var_dump($book);
+$stmt = $pdo->prepare('SELECT first_name, last_name FROM book_authors ba LEFT JOIN authors a ON ba.author_id = a.id WHERE book_id = :book_id;');
+$stmt->execute(['book_id' => $id]);
+$authors = $stmt->fetchAll();
+
+// var_dump($authors);
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?= $book['title']; ?></title>
+</head>
+<body>
+    <h1><?= $book['title']; ?></h1>
+
+    Autorid:
+    <ul>
+<?php foreach ( $authors as $author ) { ?>
+    <li><?= "{$author['first_name']} {$author['last_name']}"; ?></li>
+<?php } ?>
+    </ul>
+
+    <a href="./edit.php">Muuda</a>
+
+</body>
+</html>
